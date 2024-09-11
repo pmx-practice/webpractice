269e640a58a4dc55af1906341bf2e99fb956f219
Danilaj <daniilzizdan47@gmail.com>
2024-09-11 19:31:52 +0300

    split one file into many

--- index.php
+++ index.php
@@ -1,15 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Elite Realty SRL</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;700&display=swap" rel="stylesheet">
-
-    <!-- Подключаем библиотеку иконок Flaticon -->
-    <link href="https://cdn.jsdelivr.net/npm/@flaticon/flaticon-uicons/css/all/all.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
+    <?php include'header.php'?>
 </head>
 <body>
 <div>
