8429f78d5bf4783098297a6230b4f06f8467ec40
chayok17 <irlanahristian@gmail.com>
2024-09-11 14:49:50 +0300

    deleted all project

--- includes/header.php
+++
@@ -1,8 +0,0 @@
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tea Information - Bootstrap Black & White Design</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <link href="css/styles.css" rel="stylesheet">
-</head>
