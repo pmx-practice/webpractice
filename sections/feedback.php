581f970d37c7b1e02cb60d859d0a27bc8612e44a
chayok17 <irlanahristian@gmail.com>
2024-09-12 16:56:46 +0300

    nav buttons upd

--- sections/feedback.php
+++ sections/feedback.php
@@ -23,11 +23,12 @@
                 </div>
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselTestimonials" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="carousel-control-prev-icon" style="background-color: black;" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
             </button>
+
             <button class="carousel-control-next" type="button" data-bs-target="#carouselTestimonials" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="carousel-control-next-icon" style="background-color: black;" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
             </button>
         </div>
