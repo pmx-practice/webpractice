240c3a5eab47072660b1a76c98f7ae4f3b68656a
chayok17 <irlanahristian@gmail.com>
2024-09-12 13:50:13 +0300

    no comments

--- sections/footer.php
+++ sections/footer.php
@@ -4,7 +4,7 @@
             <div class="row">
                 <div class="col-md-4">
                     <h4 class="mb-4">LOCATE US ON MAP</h4>
-                    <!-- Встраиваем карту Google с новым адресом -->
+                
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2718.2116556824664!2d28.852709476000582!3d47.01395792996266!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40c97c33f3e2d4a5%3A0x7d6b8fbcfae6460!2sStrada%20Alexandru%20cel%20Bun%2085%2C%20Chi%C8%99in%C4%83u%2C%20Moldova!5e0!3m2!1sen!2s!4v1694534984826!5m2!1sen!2s" width="100%" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
                 <div class="col-md-4">
