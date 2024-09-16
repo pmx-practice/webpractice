c4f8e74366677b2f8f8191b3a871a3ac715b77c5
chayok17 <irlanahristian@gmail.com>
2024-09-16 11:43:14 +0300

    style ubran

--- sections/STA.php
+++ sections/STA.php
@@ -1,7 +1,7 @@
-<section class="cta-section py-5" style="background-color: #fdd835; color: #333;">
+<section class="cta-section py-5 bg-warning text-dark">
     <div class="container text-center">
-        <h2 class="mb-4 fw-bold" style="font-size: 2.5rem;">Хотите узнать больше?</h2>
-        <p class="mb-4" style="font-size: 1.2rem;">Запишитесь на бесплатную консультацию с нашими экспертами</p>
+        <h2 class="mb-4 fw-bold display-4">Хотите узнать больше?</h2>
+        <p class="mb-4 lead">Запишитесь на бесплатную консультацию с нашими экспертами</p>
         <button class="btn btn-lg btn-outline-dark px-4 py-3 rounded-pill" data-bs-toggle="modal" data-bs-target="#askQuestionModal">
             Записаться сейчас
         </button>
--- sections/footer.php
+++ sections/footer.php
@@ -4,8 +4,7 @@
             <div class="row">
                 <div class="col-md-4">
                     <h4 class="mb-4">LOCATE US ON MAP test</h4>
-                
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2718.2116556824664!2d28.852709476000582!3d47.01395792996266!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40c97c33f3e2d4a5%3A0x7d6b8fbcfae6460!2sStrada%20Alexandru%20cel%20Bun%2085%2C%20Chi%C8%99in%C4%83u%2C%20Moldova!5e0!3m2!1sen!2s!4v1694534984826!5m2!1sen!2s" width="100%" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2718.2116556824664!2d28.852709476000582!3d47.01395792996266!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40c97c33f3e2d4a5%3A0x7d6b8fbcfae6460!2sStrada%20Alexandru%20cel%20Bun%2085%2C%20Chi%C8%99in%C4%83u%2C%20Moldova!5e0!3m2!1sen!2s!4v1694534984826!5m2!1sen!2s" class="w-100 mb-4" height="250" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
                 <div class="col-md-4">
                     <h4 class="mb-4">CONTACT US</h4>
@@ -27,8 +26,8 @@
                 </div>
             </div>
         </div>
-        <div class="footer-bottom py-3 mt-3 text-center" style="background-color: #8AB547;">
-            <p>&copy; 2024 Elite Realty SRL. All rights reserved.</p>
+        <div class="footer-bottom py-3 mt-3 text-center bg-success">
+            <p class="mb-0">&copy; 2024 Elite Realty SRL. All rights reserved.</p>
         </div>
     </footer>
 </section>
