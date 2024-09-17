355167e0cfa945a1e33fe89a10c629f5a9434b24
Danilaj <daniilzizdan47@gmail.com>
2024-09-17 16:17:44 +0300

    add section about speaker and fix key values block

---
+++ sections/about_sp.php
@@ -0,0 +1,13 @@
+<div class="container border-none rounded ">
+ <div class="aboutsp-section">
+    <div class="img">
+        <img src="https://img.freepik.com/free-photo/portrait-successful-man-having-stubble-posing-with-broad-smile-keeping-arms-folded_171337-1267.jpg?ga=GA1.1.961539177.1726495942&semt=ais_hybrid">
+    </div>
+    <div class="text p-4">
+        <h1>Become a speaker today</h1>
+        <h5>Share your vision and experience with thousands of people from across the world. Theory is great - but we want tangible actions that people can take away.</h5>
+        <button type="button" class="btn btn-sp btn-info">Информация</button>
+    </div>
+   
+ </div>
+</div>
